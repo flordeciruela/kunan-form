e29b356b583a5b1d9345ee467e7e838b941ed3a2
flordeciruela <wendy.reyessan@gmail.com>
2017-05-04 02:07:27 -0500

    editando validaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,21 +4,22 @@ var inputFile = document.getElementsByName('file');
 var guardar = document.getElementById('submit');
 var form = document.getElementById('form');
 var ruc = document.getElementById('ruc');
-
+/*
 guardar.addEventListener('click',function(event) {
   event.preventDefault();
 	validarGenero();
 	validarRegPublic();
 	validarFile();
-});
 
+});
+*/
 
 function validarGenero() {
 	if(genero[0].checked || genero[1].checked) {
 		submit.nextElementSibling.innerText= "";
 		document.getElementById("formulario").reset();  // limpiar formulario
 	} else {
-		submit.nextElementSibling.innerText= "Selecciona un género.";
+		window.location = "finish.html";
 	}
 }
 
